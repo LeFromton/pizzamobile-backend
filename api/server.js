1e4127feca46d71680119075e8cdea9b724c64e4
Geronimo Weibel <geronimomatteoleon.weibel@hefr.ch>
2022-04-04 20:21:32 +0200

    Modify DB from MariaDB to MongoDB

--- api/server.js
+++ api/server.js
@@ -1,120 +1,141 @@
 // Module requirements
 const express = require('express')
-const mysql = require('mysql')
-const mysqlConfig = require('./config.js');
-const bodyParser = require("body-parser");
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser")
 
-// DB Connection
-const db = mysql.createConnection(mysqlConfig);
-db.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+const { Schema } = mongoose
+
+const orderSchema = new Schema({
+  name: String,
+  phone: String,
+  status: String,
+  pizzas: String
+})
+
+
+const Order = mongoose.model('Order', orderSchema)
+
+// Try DB Connection
+try {
+  mongoose.connect('mongodb://localhost:27017/PizzaMobileDB')
+  mongoose.connection.on('open', function (ref) {
+    console.log('Connected to mongo server.');
+  })
+} catch (error) {
+  throw error
+}
 
 // HTTPD initiation
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+const app = express()
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }))
 
-
-app.get('/', function(req, res){
-  res.send("Hello world!");
-});
-
-app.get('/api/orders', function(req, res){
-  if(req.query.status){
-    var sql = "SELECT * FROM Orders WHERE Status = ?";
-  } else {
-    var sql = "SELECT * FROM Orders"
-  }
-  db.query(sql, [req.query.status], function (err, result){
-    if (err){
-      res.sendStatus(418);
-      throw err;
-    } else {
-      res.send(result);
-    };
-  });
-});
-
-app.post('/api/kitchen/new', (req, res, next) => {
-  let values = [];
-  req.body['ctRoot'].forEach(order => {
-    let value = [order['name'], order['phone'], order['pizzas'], 'new'];
-    console.log(value);
-    values.push(value);
-  });
-  db.query("INSERT INTO Orders (Name, Phone, Pizzas, Status) VALUES ?", [values], function (err, result){
-    if (err){
-      res.sendStatus(418);
-      throw err;
-    } else {
-      res.sendStatus(200);
-    };
-  });
-});
-
-app.post('/api/kitchen/confirm', (req, res, next) => {
-  let values = [];
-  req.body['ctRoot'].forEach(order => {
-    let value = [order['id']];
-    values.push(value);
-  });
-  db.query("UPDATE Orders SET Status = 'paid' WHERE Id = ?", [values], function (err, result){
-    if (err){
-      res.sendStatus(418);
-      throw err;
-    } else {
-      res.sendStatus(200);
-    };
-  });
-});
-
-app.post('/api/kitchen/confirm-oven', (req, res, next) => {
-  let values = [];
-  req.body['ctRoot'].forEach(order => {
-    let value = [order['id']];
-    values.push(value);
-  });
-  db.query("UPDATE Orders SET Status = 'cooking' WHERE Id = ?", [values], function (err, result){
-    if (err){
-      res.sendStatus(418);
-      throw err;
-    } else {
-      res.sendStatus(200);
-    };
-  });
-});
-
-app.post('/api/kitchen/finish-oven', (req, res, next) => {
-  let values = [];
-  req.body['ctRoot'].forEach(order => {
-    let value = [order['id']];
-    values.push(value);
-  });
-  db.query("UPDATE Orders SET Status = 'finished' WHERE Id = ?", [values], function (err, result){
-    if (err){
-      res.sendStatus(418);
-      throw err;
-    } else {
-      res.sendStatus(200);
-    };
-  });
-});
-
-app.post('/api/kitchen/hand-off', (req, res, next) => {
-  req.body['ctRoot'].forEach(order => {
-    db.query("INSERT INTO LogOrders SELECT Id, Name, Phone, Pizzas FROM Orders WHERE Id=?", [order.id], function (err, result){
-      db.query("DELETE FROM Orders WHERE Id=?", [order.id], function (err, result){
-        if (err){
-          res.sendStatus(418);
-          throw err;
+// Get filtered orders (for the views)
+app.route('/api/orders/filters/:filter')
+  .get((req, res) => {
+    try {
+      Order.find({ status: req.params.filter}).lean().exec(function (error, orders) {
+        if (order == null) {
+          res.sendStatus(400)
         } else {
-          res.sendStatus(200);
-        };
-      });
-    });
-  });
-});
+          res.send(orders)
+        }
+      })
+    } catch (error) {
+      res.sendStatus(400)
+      throw error
+    }
+  })
 
-app.listen(3000);
+app.route('/api/orders')
+// Return all orders
+  .get((req, res) => {
+    try {
+      Order.find().lean().exec((errors, orders) => {
+        if (orders.length == 0) {
+          res.sendStatus(400)
+        } else {
+          res.send(JSON.stringify(orders))
+        }
+      })
+    } catch (error) {
+      res.sendStatus(400)
+      throw error
+    }
+  })
+  // New Orders
+  .post((req, res) => {
+    try {
+      let order = {
+        name: req.body.name,
+        phone: req.body.phone,
+        status: req.body.status,
+        pizzas: req.body.pizzas
+      }
+      Order.create(order)
+      res.sendStatus(201)
+    } catch (error) {
+      res.sendStatus(400)
+      throw error
+    }
+  })
+
+app.route('/api/orders/:orderId')
+  // Return specific order
+  .get((req, res) => {
+    try {
+      Order.findById({ _id: req.params.orderId}).lean()
+      .exec((error, order) => {
+        if (order == null) {
+          res.sendStatus(400)
+        } else {
+          res.send(JSON.stringify(order))
+        }
+      })
+    } catch (error) {
+      res.sendStatus(400)
+      throw error
+    }
+  })
+
+  // Update specific order(s)
+  .put((req, res) => {
+    try {
+      let filter = { _id: req.params.orderId }
+      let update = {
+        name: req.body.name,
+        phone: req.body.phone,
+        status: req.body.status,
+        pizzas: req.body.pizzas
+      }
+      Order.findOneAndUpdate(filter, update, { new: true })
+        .lean().exec((errors, order) => {
+          if (order == null) {
+            res.sendStatus(400)
+          } else {
+            res.sendStatus(202)
+          }
+      })
+    } catch (error) {
+      res.sendStatus(400)
+      throw error
+    }
+  })
+
+  // Remove specific order(s)
+  .delete((req, res) => {
+    try {
+      let filter = { _id: req.params.orderId }
+      Order.findOneAndDelete(filter)
+        .lean().exec((error) => {
+          res.sendStatus(202)
+      })
+    } catch (error) {
+      res.sendStatus(400)
+      throw error
+    }
+  })
+
+app.listen(3000)
